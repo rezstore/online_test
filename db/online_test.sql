677b5dc3925f9143a469699dfcc35fbd4d73ec3e
rohman ahmad <rohmanmail@gmail.com>
2014-10-01 16:18:32 +0700

    f

---
+++ db/online_test.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 01, 2014 at 05:17 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `online_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choice_answers`
+--
+
+CREATE TABLE IF NOT EXISTS `choice_answers` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_ID` varchar(20) NOT NULL,
+  `a` tinytext NOT NULL,
+  `b` tinytext NOT NULL,
+  `c` tinytext NOT NULL,
+  `d` tinytext NOT NULL,
+  `e` tinytext NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_options`
+--
+
+CREATE TABLE IF NOT EXISTS `class_options` (
+  `class_code` varchar(3) NOT NULL,
+  `class_name` varchar(5) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`class_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class_options`
+--
+
+INSERT INTO `class_options` (`class_code`, `class_name`, `status`) VALUES
+('ix', 'IX', 1),
+('vi', 'VI', 1),
+('vii', 'VII', 1),
+('x', 'X', 1),
+('xi', 'XI', 1),
+('xii', 'XII', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `correct_answer`
+--
+
+CREATE TABLE IF NOT EXISTS `correct_answer` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_ID` varchar(20) NOT NULL,
+  `answer` varchar(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_questions`
+--
+
+CREATE TABLE IF NOT EXISTS `exam_questions` (
+  `exam_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_code` varchar(20) NOT NULL,
+  `class_code` varchar(5) NOT NULL,
+  `exam_content` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`exam_ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `exam_questions`
+--
+
+INSERT INTO `exam_questions` (`exam_ID`, `subject_code`, `class_code`, `exam_content`, `status`) VALUES
+(1, 'mat', 'vii', 'dasdasdasdasd', 1),
+(2, 'mat', 'viii', 'asdasdasdaa123', 1),
+(3, 'mat', 'vii', 'dasdasdasdasd3123123', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_code` varchar(20) NOT NULL,
+  `subject_name` varchar(20) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`ID`, `subject_code`, `subject_name`, `status`) VALUES
+(1, 'mat', 'matematika', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `user_accounts` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `user_accounts`
+--
+
+INSERT INTO `user_accounts` (`ID`, `username`, `email`, `status`) VALUES
+(3, 'rohman', 'rohman@rezstore.com', 0),
+(4, 'rohman', 'rohman@rezstore.com', 0),
+(5, 'rohman', 'rohmanahmad123@yahoo.com', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
